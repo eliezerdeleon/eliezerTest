b2c83e08d9e16499081a9827e0e8ec03891afd40
eliezerdeleon <djnb.nickyblast@gmail.com>
2022-04-21 08:00:45 -0400

    tratando de solucionar

--- app/src/test/java/eliezerTest/runner/Runner.java
+++ app/src/test/java/eliezerTest/runner/Runner.java
@@ -2,14 +2,19 @@ package eliezerTest.runner;
 
 import org.junit.runner.RunWith;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/Features", glue = "steps"
+@CucumberOptions(
+features = "app/src/test/resources/Features",
+glue = "app/src/test/java/eliezerTest/steps"
+
+
 
 )
 
-public class Runner {
+public class runner{
 
+    
 }
